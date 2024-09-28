940aa3dd7ab140f16245dfc986f8bb5e7556832e
wngktjd13 <seonguk3553@kookmin.ac.kr>
2024-09-29 01:12:51 +0900

    feat: Layout 퍼블리싱

--- src/component/page/CommunityPage.jsx
+++ src/component/page/CommunityPage.jsx
@@ -1,17 +1,29 @@
 import React from "react";
-import styled from "styled-components"
-import Header from "../ui/Header"
+import styled from "styled-components";
+import Layout from '../../Layout';
 
 const Container = styled.div`
-    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 20px 40px;
+    background-color: #F7F0EB;
 `;
 
+const ButtonArea = styled.div`
+
+`;
+
+
 function CommunityPage() {
-    return(
-        <Container>
-            <Header/>
-        </Container>
+    return (
+        <Layout>
+            <Container>
+                <ButtonArea>
+                    
+                </ButtonArea>
+            </Container>
+        </Layout>
     );
-};
+}
 
 export default CommunityPage;
