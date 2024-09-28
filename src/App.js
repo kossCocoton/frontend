012f4ae5b11fa12549d7e0bf44d00d51a5066a96
김세희 <lolry0207@sookmyung.ac.kr>
2024-09-28 21:49:20 +0900

    feat: button 및 input 색상 변경

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
 		<Route index element={<OnboardingPage />} />
 		<Route path="login" element={<LoginPage />} />
 		<Route path="signup" element={<SignupPage />} />
+    
 		{/* <Route path="main/:communityid" element={<MainPage />} /> */}
 	</Routes>
   );
